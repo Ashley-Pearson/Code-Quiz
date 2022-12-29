631ea4e49b1d2a4cc08c9f128d84738832a67969
Ashley Pearson <ashleypearson@Ashleys-MacBook-Air.local>
2022-12-29 14:58:07 -0700

    update correct css file

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,23 @@
 //varibles for quiz
-
+var timerEl = document.getElementById('countdown');
 // start quiz
 
 //timer
 
-// creating questions
+function countdown() {
+    var timeLeft = 5;
 
+var timeInterval = setInterval(function () {
+    timeLeft--;
+    timerEl.textContent = timeLeft + " seconds remaining ";
+
+    if (timeLeft === 0) {
+      clearInterval(timeInterval);
+
+      displayMessage();
+    }
+
+  }, 1000);
+
+  countdown();
+// creating questions
